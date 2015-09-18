13f016b2286abd7eb02116ac94ba455da8917c64
Vimal Mistry <Vimalmistry10@gmail.com>
2015-09-18 18:11:30 +0530

    Create source.php

---
+++ Error_handling/source.php
@@ -0,0 +1,9 @@
+http://websec.io/2012/08/14/Playing-Your-Cards-Close-Error-Exception-Handling.html
+
+
+http://www.sitepoint.com/error-handling-in-php/
+
+http://www.hostingadvice.com/how-to/php-show-errors/
+
+
+http://websec.io/2012/08/14/Playing-Your-Cards-Close-Error-Exception-Handling.html
